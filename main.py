541e4986f7904aedf8ec53e36fbfb9afcd386a33
phankiet02 <tuankietvn@outlook.com>
2020-08-13 10:42:32 +0700

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import discord
 from dotenv import load_dotenv
@@ -12,12 +13,13 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    crawl_visualize.pre_pre_prework()
+    # crawl_visualize.pre_pre_prework()
+    print("Finished crawling data")
 
 @client.event
 async def on_message(message):
-    if message.content.lower() == "calender":
+    if message.content.lower() == "calendar" and not message.author.bot:
         channel = message.channel
-        await channel.send("I'm working on it...")
+        await channel.send("calendar")
 
 client.run(TOKEN)
